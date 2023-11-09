e82e56926096872b307619b6721978d2d62ecd79
Kauã Vinícius <kauaponte@hotmail.com>
2023-11-09 16:35:22 -0300

    Finalização do desafio

--- src/script.js
+++ src/script.js
@@ -10,14 +10,43 @@ function changeText(id, text) {
 // Daqui para baixo voce ira escrever
 // o código para resolver o desafio
 
-function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
+// Utilitários
+const APIURL  = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1292";
+const POKEMONS = [];
+let actual = 0;
+
+// Procurar imagem pela url
+async function fetchImage(url){
+  return await fetch(url).then((response)=>response.json())
+                  .then((data) => data.sprites.front_default);
 }
 
-function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
+// Popular array
+async function fetchPokemons(){
+  await fetch(APIURL).then((response)=>response.json())
+  .then((data)=>data.results.map((results)=> POKEMONS.push(results)));
+}
+
+// Mudar dados no frontend
+function changeData(img){
+  changeImage("img_sprite_front_default",img);
+  changeText("name",POKEMONS[actual].name)
+}
+
+async function previousPokemon() {
+  if(POKEMONS.length == 0)
+    await fetchPokemons();
+
+  actual == 0 ? actual = 1291 : actual--;
+
+  changeData(await fetchImage(POKEMONS[actual].url));
+}
+
+async function nextPokemon() {
+  if(POKEMONS.length == 0)
+    await fetchPokemons();
+
+  actual == 1291 ? actual = 0 : actual++;
+
+  changeData(await fetchImage(POKEMONS[actual].url));
 }
